d9798f2d8ac9b92fbbee7885a6b88b3bc87687fb
chipatredhat <chip@redhat.com>
2024-05-15 13:59:19 -0700

    Create node1_prep.sh

---
+++ files/node1_prep.sh
@@ -0,0 +1,6 @@
+sudo dnf -y tmux
+echo "tmux new-session \; split-window -v ssh node2 \; split-window -v ssh node3 \; attach" > ~/3mux.sh
+chmod +x ~/3mux.sh
+echo "bind -n C-x setw synchronize-panes" >> ~/.tmux.conf
+curl -Ls red.ht/RHELHAdemo-key > ~/.ssh/demo
+chmod 600 ~/.ssh/demo
