ae08572d4c6ce822c81a1b626e244b54c414882a
chipatredhat <chip@redhat.com>
2024-05-15 15:16:57 -0700

    Update nodes_prep.sh

--- files/nodes_prep.sh
+++ files/nodes_prep.sh
@@ -1,1 +1,3 @@
+# This script will simply add the demo public key to authorized_keys and add the file that should have already been copied across with the hostnames and IP address to the end of /etc/hosts to allow name resolution
 curl -Ls red.ht/RHELHAdemo-pub >> ~/.ssh/authorized_keys
+cat /tmp/hosts | sudo tee -a /etc/hosts
