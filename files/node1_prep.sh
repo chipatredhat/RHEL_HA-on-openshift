3eac5faf79b87f534b8535fa5fd3fafc9c51bb0a
chipatredhat <chip@redhat.com>
2024-05-16 16:12:37 -0700

    Update node1_prep.sh

--- files/node1_prep.sh
+++ files/node1_prep.sh
@@ -1,4 +1,4 @@
-sudo dnf -y tmux
+sudo dnf -y install tmux
 echo "tmux new-session \; split-window -v ssh node2 \; split-window -v ssh node3 \; attach" > ~/3mux.sh
 chmod +x ~/3mux.sh
 echo "bind -n C-x setw synchronize-panes" >> ~/.tmux.conf
