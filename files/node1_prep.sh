39adf2852c107535f37783fb06f472e863a8aaff
chipatredhat <chip@redhat.com>
2024-05-17 10:23:01 -0700

    Update node1_prep.sh

--- files/node1_prep.sh
+++ files/node1_prep.sh
@@ -1,5 +1,5 @@
 sudo dnf -y install tmux
-echo "tmux new-session \; split-window -v ssh node2 \; split-window -v ssh node3 \; attach" > ~/3mux.sh
+echo "tmux new-session \; split-window -v ssh node2 -i ~/.ssh/demo \; split-window -v ssh node3 -i ~/.ssh/demo \; attach" > ~/3mux.sh
 chmod +x ~/3mux.sh
 echo "bind -n C-x setw synchronize-panes" >> ~/.tmux.conf
 curl -Ls red.ht/RHELHAdemo-key > ~/.ssh/demo
